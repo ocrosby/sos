493318bb3f13e46ad7b3c5ccdd84625cde4e073a
Omar Crosby <omar.crosby@gmail.com>
2024-10-18 05:43:08 -0400

    chore: wip

--- sos/__main__.py
+++ sos/__main__.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-
 from bs4 import BeautifulSoup
-
 from sos.constants import TURNOUT_URL
 
 
 def fetch_data(url):
     # Set up headless Chrome options
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
@@ -23,6 +24,27 @@ def fetch_data(url):
     try:
         # Fetch the page
         driver.get(url)
+
+        # Switch to the iframe by its title
+        WebDriverWait(driver, 20).until(
+            EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@title='Data Hub - Voter Registration']"))
+        )
+
+        # Wait until the button is visible
+        WebDriverWait(driver, 20).until(
+            EC.visibility_of_element_located((By.ID, "AbsenteeBallots"))
+        )
+
+        # Scroll the button into view
+        button = driver.find_element(By.ID, "AbsenteeBallots")
+        driver.execute_script("arguments[0].scrollIntoView(true);", button)
+
+        # Wait until the button is clickable
+        WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.ID, "AbsenteeBallots"))
+        )
+
+        button.click()
 
         # Get the page source
         data = driver.page_source
